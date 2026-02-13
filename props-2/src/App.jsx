6851ead8da2ec4241af888cd65240c6b09ec96c5
Nileshnhe <nehenilesh111@gmail.com>
2026-02-13 21:33:50 +0530

    Practice React props and component data passing

---
+++ props-2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './components/card'
+
+const App = () => {
+  
+  const arr = [20,23,50,60]
+  return (
+    <div className='px-10 py-10 bg-black h-screen'>
+      
+      <Card user='sarthak'/>
+    </div>
+  )
+}
+
+export default App
---
+++ props-2/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = (props) => {
+  console.log(props)
+  return (
+    <div className='bg-white px-3 rounded-2xl py-3 w-fit m-2 '>
+     <h1>Card</h1>
+      </div>
+  )
+}
+
+export default Card
